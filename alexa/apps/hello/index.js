06c89715dcab06337a10498575093b0e02952c06
Patrick White <byelipk@kso2.local>
2017-09-03 14:32:47 -0400

    Say hello

--- alexa/apps/hello/index.js
+++ alexa/apps/hello/index.js
@@ -7,24 +7,47 @@ const hello = chatskills.app("hello");
 
 // Create a launch
 hello.launch((req, res) => {
+  res.session('trips-remaining', 5);
+
   res.say("Ask me to say hi!");
 
   // Keep session open
   res.shouldEndSession(false);
 });
 
+// Create a dictionary
+hello.dictionary = { locations: [ "beach", "mountain", "train", "plane", "couch" ] };
+
 // Create an intent.
 hello.intent(
   "hello",
   {
-    slots: {},
+    slots: {
+      Location: 'LOCATION'
+    },
     utterances: [
-      "{to |}{say|speak|tell me} {hi|hello|howdy|hi there|hiya|hi ya|hey|hay|heya}"
+      "{to |}{say|speak|tell me} {hi|hello|howdy|hi there|hiya|hi ya|hey|hay|heya}",
+      "{please |}{say aloha}",
+      "{sex |}{on the }{-|Location}"
     ]
   },
   function(req, res) {
-    res.say("Hello, World!");
-    res.shouldEndSession(false);
+    const location = req.slot('Location');
+    let num = req.session('trips-remaining');
+
+    num--;
+
+    res.session('trips-remaining', num);
+
+    res.say(`Hello, World (from the ${location})!`);
+    res.say(`${num} trips remaining...`);
+
+    if (num === 0) {
+      res.shouldEndSession(true);
+    }
+    else {
+      res.shouldEndSession(false);
+    }
   }
 );
 
@@ -38,6 +61,8 @@ const rl = readline.createInterface({
 });
 
 chatskills.launch(hello);
+
+console.log(hello.utterances());
 
 rl.prompt();
 
